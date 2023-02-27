74b313f2441e53335012ce92f295f1cd4e31aee1
AndreyKarapetyan <andrey.karapetyan2000@mail.ru>
2023-02-27 17:01:28 +0400

    Initial commit

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,13 +3,25 @@ import styles from './app.module.scss';
 
 import NxWelcome from './nx-welcome';
 
+import { Route, Routes, Link } from 'react-router-dom';
+import Login from './screens/Login';
+
 export function App() {
   return (
-    <>
-      <NxWelcome title="invest-fe" />
-
-      <div />
-    </>
+      <Routes>
+        <Route
+          path="/login"
+          element={<Login/>}
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
   );
 }
 
