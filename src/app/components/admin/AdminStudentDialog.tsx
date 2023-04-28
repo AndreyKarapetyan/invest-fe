746740a5e37f2b2a0d5f854a9b6c645f0bc6851f
AndreyKarapetyan <andrey.karapetyan2000@mail.ru>
2023-04-29 00:35:08 +0400

    Added form validation for Admin students page

--- src/app/components/admin/AdminStudentDialog.tsx
+++ src/app/components/admin/AdminStudentDialog.tsx
@@ -4,14 +4,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import IconButton from '@mui/material/IconButton';
-import {
-  CircularProgress,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from '@mui/material';
+import { CircularProgress, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import {
   formFieldStyles,
   StyledAutoComplete,
@@ -21,6 +14,7 @@ import {
 } from './styled/student-dialog';
 import { Fragment, useState } from 'react';
 import { StudentStatus } from '../../types/student';
+import { isEmail, isInt, isPositive } from 'class-validator';
 
 interface AdminDialogProps {
   isOpen: boolean;
@@ -52,23 +46,63 @@ export function AdminStudentDialog({
       status: StudentStatus.Pending,
       actualFee: null,
       formalFee: null,
+      email: null,
       groupId: null,
       groupName: null,
       teacherId: null,
-    }
+    },
   );
+  const [formErrors, setFormErrors] = useState({
+    name: false,
+    lastname: false,
+    status: false,
+    actualFee: false,
+    formalFee: false,
+    email: false,
+    groupId: false,
+    groupName: false,
+    teacherId: false,
+  });
+  const validationFuncMapping = {
+    name: (name: any) => name,
+    lastname: (lastname: any) => lastname,
+    actualFee: (actualFee: any) => actualFee && isPositive(Number(actualFee)) && isInt(Number(actualFee)),
+    formalFee: (formalFee: any) => formalFee && isPositive(Number(formalFee)) && isInt(Number(formalFee)),
+    email: (email: any) => !email || isEmail(email),
+    status: (status: any) => Object.values(StudentStatus).includes(status),
+    teacherId: (teacherId: any, groupId?: any, groupName?: any) =>
+      !teacherId || (teacherId && groupId) || (teacherId && groupName),
+    groupName: (groupName: any) =>
+      !studentData.teacherId || (studentData.groupId && !groupName) || (!studentData.groupId && groupName),
+    groupId: (groupId: any) =>
+      !studentData.teacherId || (studentData.groupName && !groupId) || (!studentData.groupName && groupId),
+  };
   const [teacherOptionsOpen, setTeacherOptionsOpen] = useState(false); // For circular progress
   const [groupOptionsOpen, setGroupOptionsOpen] = useState(false);
   const [isNewGroup, setIsNewGroup] = useState(!studentData.teacherId);
-  const [groupFieldOpen, setGroupFieldOpen] = useState(
-    Boolean(studentData.teacherId)
-  );
+  const [groupFieldOpen, setGroupFieldOpen] = useState(Boolean(studentData.teacherId));
 
   const handleStudentDataChange = (key: string, value: any) => {
     setStudentData((curStudent: any) => ({
       ...curStudent,
       [key]: value,
     }));
+    setFormErrors((curErrors) => ({
+      ...curErrors,
+      [key]: !validationFuncMapping[key as keyof typeof validationFuncMapping](value),
+    }));
+    if (key === 'groupId') {
+      setFormErrors((curErrors) => ({
+        ...curErrors,
+        teacherId: !validationFuncMapping.teacherId(studentData.teacherId, value),
+      }));
+    }
+    if (key === 'groupName') {
+      setFormErrors((curErrors) => ({
+        ...curErrors,
+        teacherId: !validationFuncMapping.teacherId(studentData.teacherId, null, value),
+      }));
+    }
   };
 
   const handleTeacherOpen = () => {
@@ -84,9 +118,9 @@ export function AdminStudentDialog({
       handleStudentDataChange('teacherId', option.id);
       getTeacherGroups(option.id);
     } else {
-      handleStudentDataChange('teacherId', null);
       handleStudentDataChange('groupId', null);
       handleStudentDataChange('groupName', null);
+      handleStudentDataChange('teacherId', null); // Order is important
       setGroupFieldOpen(false);
     }
   };
@@ -123,6 +157,27 @@ export function AdminStudentDialog({
     handleStudentDataChange(name, value || null);
   };
 
+  const onSaveChanges = () => {
+    const { name, lastname, actualFee, formalFee, email, teacherId, status, groupId, groupName } = studentData;
+    const errors: Partial<typeof formErrors> = {
+      name: !validationFuncMapping.name(name),
+      lastname: !validationFuncMapping.lastname(lastname),
+      actualFee: !validationFuncMapping.actualFee(actualFee),
+      formalFee: !validationFuncMapping.formalFee(formalFee),
+      email: !validationFuncMapping.email(email),
+      status: !validationFuncMapping.status(status),
+      teacherId: !validationFuncMapping.teacherId(teacherId, groupId, groupName),
+      groupId: !validationFuncMapping.groupId(groupId),
+      groupName: !validationFuncMapping.groupName(groupName),
+    };
+    setFormErrors(errors as any);
+    const areErrors = Object.values(errors).some((err) => err);
+    console.log(errors, Object.values(errors), areErrors);
+    if (!areErrors) {
+      handleSubmit(studentData);
+    }
+  };
+
   return (
     <StyledDialog open={isOpen} onClose={handleClose}>
       <DialogTitle>
@@ -148,6 +203,8 @@ export function AdminStudentDialog({
             value={studentData.name}
             variant="outlined"
             required
+            error={formErrors.name}
+            helperText={formErrors.name ? 'Name must not be empty' : ''}
             onChange={onInputChange}
           />
           <StyledTextField
@@ -156,6 +213,8 @@ export function AdminStudentDialog({
             value={studentData.lastname}
             variant="outlined"
             required
+            error={formErrors.lastname}
+            helperText={formErrors.lastname ? 'Lastname must not be empty' : ''}
             onChange={onInputChange}
           />
           <StyledTextField
@@ -164,6 +223,8 @@ export function AdminStudentDialog({
             value={studentData.formalFee}
             variant="outlined"
             required
+            error={formErrors.formalFee}
+            helperText={formErrors.formalFee ? 'Formal fee must be a positive integer number' : ''}
             onChange={onInputChange}
           />
           <StyledTextField
@@ -172,6 +233,8 @@ export function AdminStudentDialog({
             value={studentData.actualFee}
             variant="outlined"
             required
+            error={formErrors.actualFee}
+            helperText={formErrors.actualFee ? 'Actual fee must be a positive integer number' : ''}
             onChange={onInputChange}
           />
           <StyledTextField
@@ -179,9 +242,11 @@ export function AdminStudentDialog({
             label="Email"
             value={studentData.email}
             variant="outlined"
+            error={formErrors.email}
+            helperText={formErrors.email ? 'Invalid email' : ''}
             onChange={onInputChange}
           />
-          <StyledFormControl>
+          <StyledFormControl required>
             <InputLabel id="status-label">Status</InputLabel>
             <Select
               labelId="status-label"
@@ -200,23 +265,19 @@ export function AdminStudentDialog({
             onChange={handleTeacherChange}
             options={(!teachersLoading && teachers) || []}
             loading={teachersLoading}
-            getOptionLabel={(option: any) =>
-              `${option.name} ${option.lastname}`
-            }
-            value={
-              teachers.find((t: any) => t.id === studentData.teacherId) || null
-            }
+            getOptionLabel={(option: any) => `${option.name} ${option.lastname}`}
+            value={teachers.find((t: any) => t.id === studentData.teacherId) || null}
             renderInput={(params) => (
               <TextField
                 {...params}
                 label="Teacher"
+                error={formErrors.teacherId}
+                helperText={formErrors.teacherId ? 'Please either create a new group or select existing' : ''}
                 InputProps={{
                   ...params.InputProps,
                   endAdornment: (
                     <Fragment>
-                      {teachersLoading && teacherOptionsOpen ? (
-                        <CircularProgress color="inherit" size={20} />
-                      ) : null}
+                      {teachersLoading && teacherOptionsOpen ? <CircularProgress color="inherit" size={20} /> : null}
                       {params.InputProps.endAdornment}
                     </Fragment>
                   ),
@@ -249,6 +310,8 @@ export function AdminStudentDialog({
                 label="New Group Name"
                 variant="outlined"
                 required
+                error={formErrors.groupName}
+                helperText={formErrors.groupName ? 'Group must not be empty' : ''}
                 onChange={onInputChange}
               />
             ) : (
@@ -259,20 +322,18 @@ export function AdminStudentDialog({
                 options={(!groupsLoading && groups) || []}
                 loading={groupsLoading}
                 getOptionLabel={(option: any) => option.name}
-                value={
-                  groups.find((g: any) => g.id === studentData.groupId) || null
-                }
+                value={groups.find((g: any) => g.id === studentData.groupId) || null}
                 renderInput={(params) => (
                   <TextField
                     {...params}
                     label="Existing Group Name"
+                    error={formErrors.groupId}
+                    helperText={formErrors.groupId ? 'Group must not be empty' : ''}
                     InputProps={{
                       ...params.InputProps,
                       endAdornment: (
                         <Fragment>
-                          {groupsLoading && groupOptionsOpen ? (
-                            <CircularProgress color="inherit" size={20} />
-                          ) : null}
+                          {groupsLoading && groupOptionsOpen ? <CircularProgress color="inherit" size={20} /> : null}
                           {params.InputProps.endAdornment}
                         </Fragment>
                       ),
@@ -284,7 +345,7 @@ export function AdminStudentDialog({
         </Grid>
       </DialogContent>
       <DialogActions>
-        <Button autoFocus onClick={() => handleSubmit(studentData)}>
+        <Button autoFocus onClick={onSaveChanges}>
           Save Changes
         </Button>
       </DialogActions>
