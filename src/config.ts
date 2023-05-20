7f642220dde98f106f8e4017de85ba397ea4e92f
AndreyKarapetyan <andrey.karapetyan2000@mail.ru>
2023-05-20 20:54:52 +0400

    Config change

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export let BASE_API_URL = 'http://localhost:8080/';
 if (NODE_ENV === 'local') {
   BASE_API_URL = 'http://localhost:8080/'
 } else {
-  BASE_API_URL = 'https://ip4wfnzq63.eu-west-1.awsapprunner.com/'
+  BASE_API_URL = 'https://apeqiqkwf9.eu-west-1.awsapprunner.com/'
 }
 export const USER_REFRESH_TOKEN_URL = '/refresh-token';
 
